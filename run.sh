081e1cee1e58decfc37438ebf41521e75b678a46
usouf <yousuf.m.n@gmail.com>
2023-12-28 17:11:08 +0400

    run celery with docker

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@
 /usr/local/bin/python manage.py migrate --noinput
 /usr/local/bin/python manage.py collectstatic --noinput
 
+/usr/local/bin/celery -A your_project_name worker -l info &
+/usr/local/bin/celery -A your_project_name beat -l info &
+
 RUN_PORT=${PORT:-8000}
 /usr/local/bin/gunicorn docker_test.wsgi:application --bind "0.0.0.0:${RUN_PORT}"
