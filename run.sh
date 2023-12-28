31dd3727091ccdf4af71a02d0d7dce0f09e4a922
usouf <yousuf.m.n@gmail.com>
2023-12-28 16:35:22 +0400

    fix gunicorn config

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 /usr/local/bin/python manage.py migrate --noinput
 
-/usr/local/bin/python manage.py elpsuperuser
+# /usr/local/bin/python manage.py elpsuperuser
 
 /usr/local/bin/python manage.py collectstatic --noinput
 
 RUN_PORT=${PORT:-8000}
-/usr/local/bin/gunicorn elp.wsgi:application --bind "0.0.0.0:${RUN_PORT}"
+/usr/local/bin/gunicorn docker_test.wsgi:application --bind "0.0.0.0:${RUN_PORT}"
